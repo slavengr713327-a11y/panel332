c57684d51d00b580ee077a4dd4b847960dd89ed4
slavengr713327-a11y <slavengr713327@gmail.com>
2026-05-25 03:08:18 -0700

    Add files via upload

--- app.py
+++ app.py
@@ -114,4 +114,5 @@ def create_admin():
 if __name__ == '__main__':
     with app.app_context():
         db.create_all()
-    app.run(debug=True, port=5000)
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host='0.0.0.0', port=port)
